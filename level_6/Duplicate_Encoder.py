7e2e0f8d7f466456dbc19c928799fd653344f692
nariman2022 <mardanovnariman@gmail.com>
2023-12-15 14:12:56 -0500

    added new katas

---
+++ level_6/Duplicate_Encoder.py
@@ -0,0 +1,19 @@
+# The goal of this exercise is to convert a string to a new string where each character in the new string
+# is "(" if that character appears only once in the original string, or ")" if that character appears more than once
+# in the original string. Ignore capitalization when determining if a character is a duplicate.
+#
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+
+def duplicate_encode(word):
+    word = word.lower()
+    new_word = ""
+    for i in word:
+        if word.count(i) == 1:
+            new_word += '('
+        else:
+            new_word +=')'
+    return new_word
